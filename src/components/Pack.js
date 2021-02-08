6d9e2b6722d12856f9dd1b83ec23ac02dae99238
Adam <adamadolfo8@gmail.com>
2021-02-08 15:42:11 -0500

    styling packs page

--- src/components/Pack.js
+++ src/components/Pack.js
@@ -1,7 +1,12 @@
 import React from "react"
 import FirePack from "../Fire.png"
+import background4 from "../background4.jpg"
 
 const Pack = () => {
+    const handleHover = (x) => {
+        // x.src = background4
+        console.log(x)
+    } 
 
 
     return (
